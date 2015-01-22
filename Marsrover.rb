929ae97550fa32a2b0b72fa932a4e263099bd041
Zachary Toth <zachtoth@gmail.com>
2015-01-22 14:15:43 -0500

    Rover Work in progress, figuring out input

--- Marsrover.rb
+++ Marsrover.rb
@@ -1,11 +1,68 @@
-puts "Please input new instructions being coordinates (x, y) and a direction to face (N,S,E,W)"
-instructions.getschomp
-
 class Rover
 	attr_accessor :x, :y, :direction
-	def initialze(x, y, directon)
+	def initialize(x = 5, y = 5, direction = "S")
 		@x = x
 		@y = y
 		@direction = direction
 	end
+
+	def read_instructions
+		instructions.each_char do |instruction|
+			if instruction == "M"
+				move
+			elsif instruction == "L" || instruction == "R"
+				rotate
+			else "That's not a valid command!"
+		    end
+		end
+	end
+
+	def move
+		case @direction
+		when "N"
+			then @y +=1
+		when "S"
+			then @y -=1
+		when "E"
+			then @x +=1
+		when "W"
+			then @x -=1			
+		end  
+
+	end
+
+	def rotate
+	
+		if ((@direction == "N") && (read_instructions == "L")) || ((read_instructions == "S") && (read_instructions == "R"))
+			@direction = "W" 
+
+		elsif ((@direction == "S") && (read_instructions == "L")) || ((@direction == "N") && (read_instructions == "R"))
+			@direction = "E"
+
+		elsif ((@direction == "E") && (read_instructions == "L")) || ((@direction == "W") && (read_instructions == "R"))
+			@direction = "N"
+		elsif ((@direction == "W") && (read_instructions == "L")) || ((@direction == "E") && (read_instructions == "R"))
+			@direction = "S"
+
+		end
+	end
+	def location
+		"My new location is #{@x}, #{@y}, #{@direction}"
+	end
 end
+
+# puts "Please input the plateau size"
+# plateau = gets.chomp
+
+
+puts "Give the location of the first Rover"
+roverone = gets.chomp.upcase
+puts roverone
+rover1 = Rover.new(roverone[0], roverone[2], roverone[4])
+rover1.location
+
+puts "Please input new instructions being L or R to turn left or right
+and M to move a space forward. For example LLMRM"
+instructions = gets.chomp.upcase
+# rover1.read_instructions
+rover1.location
