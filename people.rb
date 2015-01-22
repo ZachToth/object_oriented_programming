89f62ac2a5429a951d24d4bed4eb0f0a932cf13e
Zachary Toth <zachtoth@gmail.com>
2015-01-22 11:04:03 -0500

    the set up of exercise 1

---
+++ people.rb
@@ -0,0 +1,21 @@
+class Student
+  def learn 
+    "I get it!"
+  end
+end
+
+class Instructor
+  def teach
+    "Everything in Ruby is an Object!"
+  end
+end
+
+class Person
+  def initialize name
+    @name = name
+
+  def greeting
+    "Hello #{@name}"
+  end
+end
+
