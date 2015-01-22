e602f403ce23c574e56ae92c62ae8428e385d35b
Zachary Toth <zachtoth@gmail.com>
2015-01-22 11:38:06 -0500

    Final version

--- people.rb
+++ people.rb
@@ -1,21 +1,41 @@
-class Student
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name=name
+  end
+end
+
+class Student < Person
   def learn 
     "I get it!"
   end
+
+  def greeting
+    "Hello my name is #{@name}"
+  end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     "Everything in Ruby is an Object!"
   end
-end
-
-class Person
-  def initialize name
-    @name = name
-
   def greeting
-    "Hello #{@name}"
+    "Hello my name is #{@name}"
   end
 end
+
+rubyinstructor = Instructor.new("Chris")
+bitmakerstudent = Student.new("Christina")
+
+puts rubyinstructor.greeting
+puts bitmakerstudent.greeting
+
+puts rubyinstructor.teach
+puts bitmakerstudent.learn
+
+puts bitmakerstudent.teach
+
+# using the teach method with the student does not work
+# because .teach is a method that is only aviable for objects created from the 
+# Instructor class  
 
